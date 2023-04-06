af8935078a4958e6067431bb31011e9f2a84f83c
arunachalamuthu <muthuvel171998@gmail.com>
2023-04-06 18:35:56 +0530

    ss

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ io.on("connection", (socket) => {
   });
 });
 app.get("/", (req, res) => {
-  res.send("Giphy Chat Server is running successfully");
+  res.json("Giphy Chat Server is running successfully");
 });
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
